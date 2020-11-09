69e1ee80cee1b334d58b4271c5e6ab1fd4e7c40b
Mohammad Azam <azamsharp@gmail.com>
2020-11-09 13:34:15 -0600

    Initial Commit

---
+++ LetsChat/LetsChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetsChatApp.swift
+//  LetsChat
+//
+//  Created by Mohammad Azam on 11/9/20.
+//
+
+import SwiftUI
+
+@main
+struct LetsChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
